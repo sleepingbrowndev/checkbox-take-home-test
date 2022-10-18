0f916af777d940e44a1b39010870a9c233923350
Shashenta Rao <shashenta.rao@ecloudvalley.com>
2022-10-18 23:57:15 +0800

    added aurora pgsql

--- lib/checkbox-take-home-test-stack.ts
+++ lib/checkbox-take-home-test-stack.ts
@@ -1,5 +1,7 @@
 import * as cdk from '@aws-cdk/core';
 import * as appsync from '@aws-cdk/aws-appsync';
+import * as ec2 from '@aws-cdk/aws-ec2';
+import * as rds from '@aws-cdk/aws-rds';
 
 export class CheckboxTakeHomeTestStack extends cdk.Stack {
 	constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -17,6 +19,18 @@ export class CheckboxTakeHomeTestStack extends cdk.Stack {
 					}
 				},
 			},
+		});
+
+		// VPC for Aurora DB cluster
+		const vpc = new ec2.Vpc(this, 'TaskAppVPC');
+
+		// Create the Aurora DB cluster; set the engine to Postgresql
+		const cluster = new rds.ServerlessCluster(this, 'AuroraTaskCluster', {
+			engine: rds.DatabaseClusterEngine.AURORA_POSTGRESQL,
+			parameterGroup: rds.ParameterGroup.fromParameterGroupName(this, 'ParameterGroup', 'default.aurora-postgresql13'),
+			defaultDatabaseName: 'TaskDB',
+			vpc,
+			scaling: { autoPause: cdk.Duration.seconds(300) }
 		});
 	}
 }
