2a7e0c6dc2b35fe9271df77de2f3f6f8936adf3f
Shashenta Rao <shashenta.rao@ecloudvalley.com>
2022-10-18 22:52:47 +0800

    Initial commit

---
+++ test/checkbox-take-home-test.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CheckboxTakeHomeTest from '../lib/checkbox-take-home-test-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/checkbox-take-home-test-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CheckboxTakeHomeTest.CheckboxTakeHomeTestStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
